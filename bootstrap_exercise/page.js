dc2ecf9baefd32a8614b06c9d9e9143360a0bb0b
kakame132 <67035034+kakame132@users.noreply.github.com>
2020-08-11 17:39:00 +0700

    bootstrap update

---
+++ bootstrap_exercise/page.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $("#Continue1").click(function(){
+    $("#Modal1").modal();
+  });
+  $("#Continue2").click(function(){
+    $("#Modal2").modal();
+  });
+  $("#Continue3").click(function(){
+    $("#Modal3").modal();
+  });
+});
