b5d075906a10926c223aa729857765ef0fab2365
kakame132 <kakame1605@gmail.com>
2020-08-19 22:27:35 +0700

    Add validation

--- final_app/my_app/app/models/album.rb
+++ final_app/my_app/app/models/album.rb
@@ -1,4 +1,5 @@
 class Album < ApplicationRecord
+  belongs_to :users
   validates :title, presence: true, length: {maximum: 140}
   validates :description, presence: true, length: {maximum: 300}
 end
--- final_app/my_app/app/models/photo.rb
+++ final_app/my_app/app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ApplicationRecord
+  belongs_to:users
   validates :title, presence: true, length: {maximum: 140}
   validates :description, presence: true, length: {maximum: 300}
 end
