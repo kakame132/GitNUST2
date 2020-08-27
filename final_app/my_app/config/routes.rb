667be2f386508d21a88d8ebbae5ef8bfe9097bf2
kakame132 <kakame1605@gmail.com>
2020-08-27 10:50:41 +0700

    update routes

--- final_app/my_app/config/routes.rb
+++ final_app/my_app/config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+ root to: "home#guestfeed"
+ get 'discover', to: 'home#discover'
+ get 'login', to: 'home#login'
+ get 'signin', to: 'home#signin'
+ resources :users ,except: [:index, :destroy] do
+   member do
+     get 'profile', to: 'home#profile'
+  end
+  resource :photos
+  resource :albums
+ end
+ namespace :admin do
+   resources :users, shallow: true
+ end
 end
